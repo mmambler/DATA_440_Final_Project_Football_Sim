bb0dddd2fa3d2dfd36cdd90f64b98868ec49e519
mmambler <mmambler@email.wm.edu>
2023-11-27 14:46:12 -0500

    Testing

--- src/football_app.py
+++ src/football_app.py
@@ -105,27 +105,25 @@ class FBApp:
         return
     
     def update_position_rush(self):
-
-        try:
-            df = FootballDB().get_tRush()
-            filter_df = df[(df['fieldpos']==str(st.session_state.FIELD_POS)) & (df['down']==st.session_state.DOWN) & (df['distance']==st.session_state.DISTANCE)]
-            yards_gained = filter_df['yards_gained'].sample().iloc[0]
-            dist_temp = st.session_state.DISTANCE - yards_gained
-            if dist_temp > 0:
-                st.session_state.DISTANCE = dist_temp
-                if st.session_state.DOWN < 4:
-                    st.session_state.DOWN += 1
-                    field_pos_temp = dropdown.field_pos_dict[st.session_state.FIELD_POS] + yards_gained
-                    st.session_state.FIELD_POS = dropdown.field_pos_dict_reverse[field_pos_temp]
-                else:
-                    self.reset_drive()
-            else:
-                st.session_state.DOWN = 1
-                st.session_state.DISTANCE = 10
+        
+        df = FootballDB().get_tRush()
+        filter_df = df[(df['fieldpos']==str(st.session_state.FIELD_POS)) & (df['down']==st.session_state.DOWN) & (df['distance']==st.session_state.DISTANCE)]
+        yards_gained = filter_df['yards_gained'].sample().iloc[0]
+        dist_temp = st.session_state.DISTANCE - yards_gained
+        if dist_temp > 0:
+            st.session_state.DISTANCE = dist_temp
+            if st.session_state.DOWN < 4:
+                st.session_state.DOWN += 1
                 field_pos_temp = dropdown.field_pos_dict[st.session_state.FIELD_POS] + yards_gained
                 st.session_state.FIELD_POS = dropdown.field_pos_dict_reverse[field_pos_temp]
-        except:
-            self.reset_drive()
+            else:
+                self.reset_drive()
+        else:
+            st.session_state.DOWN = 1
+            st.session_state.DISTANCE = 10
+            field_pos_temp = dropdown.field_pos_dict[st.session_state.FIELD_POS] + yards_gained
+            st.session_state.FIELD_POS = dropdown.field_pos_dict_reverse[field_pos_temp]
+
         return
     
     def pass_master_update(self):
